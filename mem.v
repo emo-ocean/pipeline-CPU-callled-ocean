72a0d3326137ab4af0e076c9603dc5e01db5beba
emo-ocean <116006682+emo-ocean@users.noreply.github.com>
2023-05-16 11:36:31 +0800

    Update mem.v

--- mem.v
+++ mem.v
@@ -1,6 +1,5 @@
 //////////////////////////////////////////////////////////////////////
 ////                                                              ////
-//// Copyright (C) 2014 leishangwen@163.com                       ////
 ////                                                              ////
 //// This source file may be used and distributed without         ////
 //// restriction provided that this copyright statement is not    ////
@@ -24,9 +23,8 @@
 //////////////////////////////////////////////////////////////////////
 // Module:  mem
 // File:    mem.v
-// Author:  Lei Silei
-// E-mail:  leishangwen@163.com
-// Description: 访存阶段
+// Author:  yuzehai
+// Description: mem stage
 // Revision: 1.0
 //////////////////////////////////////////////////////////////////////
 
@@ -34,30 +32,30 @@
 
 module mem(
 
-	input wire										rst,
+	input wire			rst,
 	
-	//来自执行阶段的信息	
-	input wire[`RegAddrBus]       wd_i,
-	input wire                    wreg_i,
-	input wire[`RegBus]					  wdata_i,
+	//from ex	
+	input wire[`RegAddrBus]       des_addr_in,
+	input wire                    des_exist_in,
+	input wire[`RegBus]		des_data_in,
 	
-	//送到回写阶段的信息
-	output reg[`RegAddrBus]      wd_o,
-	output reg                   wreg_o,
-	output reg[`RegBus]					 wdata_o
+	//pass to wb
+	output reg[`RegAddrBus]      des_addr_out,
+	output reg                   des_exist_out,
+	output reg[`RegBus]		 des_data_out
 	
 );
 
 	
 	always @ (*) begin
 		if(rst == `RstEnable) begin
-			wd_o <= `NOPRegAddr;
-			wreg_o <= `WriteDisable;
-		  wdata_o <= `ZeroWord;
+			des_addr_out <= `NOPRegAddr;
+			des_exist_out <= `WriteDisable;
+		  	des_data_out <= `ZeroWord;
 		end else begin
-		  wd_o <= wd_i;
-			wreg_o <= wreg_i;
-			wdata_o <= wdata_i;
+		  	des_addr_out <= des_addr_in;
+			des_exist_out <= des_exist_in;
+			des_data_out <= des_data_in;
 		end    //if
 	end      //always
 			
